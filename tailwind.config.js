77685c858ebca967f7938d3e7b7723910c4f2e90
Nurnaby Siddiqui <nurnaby.siddiqui@selisegroup.com>
2024-09-15 19:16:26 +0600

    map issue env setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,44 @@ module.exports = {
   content: [
     "./*.{html,php,js}",
     "./**/*.php",
+    // "node_modules/preline/dist/*.js",
     // "!./node_modules/**/*"
   ],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
-      lg: "992px",
-      xl: "1200px",
-      "2xl": "1280px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
     },
     extend: {
       colors: {
         primary: "var(--primary)",
         secondary: "var(--secondary)",
+        heading: "var(--heading)",
         green: {
-          dark: "var(--green-dark)",
           light: "var(--green-light)",
+        },
+        ash: {
+          dark: "var(--text)",
+          mid: "var(--ash-mid)",
+          light: "var(--ash-light)",
         },
         grey: {
           dark: "var(--grey-dark)",
           mid: "var(--grey-mid)",
           light: "var(--grey-light)",
-          border: "var(--grey-border)",
         },
       },
       fontFamily: {
         newsreader: ["Newsreader", "serif"],
         lato: ["Lato", "sans-serif"],
+        archivo: ["Archivo", "sans-serif"],
       },
+
       spacing: {},
     },
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
